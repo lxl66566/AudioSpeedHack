19217c184748d5e52229bb6431ecc7ea3b2e2217
lxl66566 <lxl66566@gmail.com>
2025-11-22 00:34:35 +0800

    feat: include v1 dlls
    Signed-off-by: lxl66566 <lxl66566@gmail.com>

--- src/asset.rs
+++ src/asset.rs
@@ -10,8 +10,35 @@ use strum::IntoEnumIterator;
 
 use crate::{
     reg,
-    utils::{self, DSOUND_DLL_NAME, MMDEVAPI_DLL_NAME, SupportedDLLs},
+    utils::{self, DSOUND_DLL_NAME, MMDEVAPI_DLL_NAME, SOUNDTOUCH_DLL_NAME, SupportedDLLs},
 };
+
+pub fn extract_soundtouch_assets(
+    system: utils::System,
+    dest: impl AsRef<Path>,
+) -> Result<Vec<PathBuf>> {
+    #[cfg(not(debug_assertions))]
+    let st_archive = NamedArchive::load(include_dir!(
+        "assets/SoundTouch",
+        compression = "zstd",
+        level = 22
+    ));
+    #[cfg(debug_assertions)]
+    let st_archive = NamedArchive::load(include_dir!("assets/SoundTouch"));
+
+    let st_bytes = st_archive
+        .get(format!("SoundTouch-{}.dll", system).as_str())
+        .unwrap();
+    let st_dest = dest.as_ref().join(SOUNDTOUCH_DLL_NAME);
+
+    let mut ret = vec![];
+    if !st_dest.exists() {
+        ret.push(st_dest.clone());
+    }
+    fs::write(st_dest, st_bytes)?;
+    info!("Extracted {SOUNDTOUCH_DLL_NAME}");
+    Ok(ret)
+}
 
 /// 提取选择的 dsound dll 到指定目录
 ///
@@ -34,23 +61,12 @@ pub fn extract_dsound_assets(
     #[cfg(debug_assertions)]
     let dsound_archive = NamedArchive::load(include_dir!("assets/dsound"));
 
-    let aldrv_bytes = dsound_archive
-        .get(format!("dsoal-aldrv-{}.dll", system).as_str())
-        .unwrap();
     let dsound_bytes: &[u8] = dsound_archive
         .get(format!("dsound-{}-{:.1}.dll", system, speed).as_str())
         .unwrap();
 
-    let aldrv_dest = dest.as_ref().join("dsoal-aldrv.dll");
     let dsound_dest = dest.as_ref().join(DSOUND_DLL_NAME);
     let mut ret = vec![];
-
-    if !aldrv_dest.exists() {
-        ret.push(aldrv_dest.clone());
-    }
-    fs::write(aldrv_dest, aldrv_bytes)?;
-    info!("Extracted dsoal-aldrv.dll");
-
     if !dsound_dest.exists() {
         ret.push(dsound_dest.clone());
     }
@@ -103,8 +119,9 @@ fn extract_specific_dll_and_reg(
     let ret = match dll {
         SupportedDLLs::DSound => extract_dsound_assets(system, speed, dest)?,
         SupportedDLLs::MMDevAPI => {
+            let tmp = extract_mmdevapi_assets(system, speed, dest)?;
             reg::registry_op(&reg::RegistryOperation::Add)?;
-            extract_mmdevapi_assets(system, speed, dest)?
+            tmp
         }
     };
     Ok(ret)
@@ -122,7 +139,7 @@ pub fn extract_selected_and_reg(
     speed: f32,
     dest: impl AsRef<Path>,
 ) -> Result<Vec<PathBuf>> {
-    let mut ret = vec![];
+    let mut ret = extract_soundtouch_assets(system, &dest)?;
     if let Some(dll) = selected {
         ret.extend(extract_specific_dll_and_reg(dll, system, speed, dest)?);
     } else {
